9017d7d6e6e4edf8d20c28fd9341db3180e7bf29
UtshavxPaudel <utshav@customerfirstai.com>
2025-04-14 00:17:20 +0545

    remaining Lab

---
+++ lab10/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10_stringhandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Welcome to cSharp MBM class A";
+            string b = "\"Welcome to CSharp MBM Class B\"";
+            string c = "\\Welcome to CSharp MBM Class C\\";
+            string d = "\t Welcome to CSharp MBM Class D";
+            string e = "One\nTwo\nThree ";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab12/lab12_float2int_explicitconversion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12_float2int_explicitconversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 34.4352f;
+            int b = (int)a;
+            //int b = Convert.ToInt32(a);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
